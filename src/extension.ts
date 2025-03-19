faf129b5be41dbd405b2c9a7de2265980e239621
mohamednaji7 <n4jidx@gmail.com>
2025-03-19 09:20:19 +0200

    initial

---
+++ src/extension.ts
@@ -0,0 +1,123 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import ollama  from 'ollama';
+import * as vscode from 'vscode';
+
+const INST_PROMPT =
+  'You are a helpful code tutor. Your job is to teach the user with simple descriptions and sample code of the concept. Respond with a guided overview of the concept in a series of messages. Do not give the user the answer directly, but guide them to find the answer themselves. If the user asks a non-programming question, politely decline to respond.';
+
+
+  
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "codawy" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('codawy.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from Codawy - Beyond LLM chats!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+// define a chat handler
+const tutorHandler: vscode.ChatRequestHandler = async (
+	request: vscode.ChatRequest,
+	context: vscode.ChatContext,
+	stream: vscode.ChatResponseStream,
+	token: vscode.CancellationToken
+  ) => {
+	// initialize the prompt
+	let prompt = INST_PROMPT;
+  
+	// initialize the messages array with the prompt
+	const messages = [vscode.LanguageModelChatMessage.User(prompt)];
+  
+	// add in the user's message
+	messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+  
+	// send the request
+	const chatResponse = await request.model.sendRequest(messages, {}, token);
+  
+	// stream the response
+	for await (const fragment of chatResponse.text) {
+	  stream.markdown(fragment);
+	}
+  
+	return;
+  };
+  
+  // create participant
+  const tutor = vscode.chat.createChatParticipant('codawy.codawy-tutor', tutorHandler);
+  
+  // add icon to participant
+//   tutor.iconPath = vscode.Uri.joinPath(context.extensionUri, 'tutor.jpeg');
+  
+  
+  // define a chat handler
+const handler: vscode.ChatRequestHandler = async (
+	request: vscode.ChatRequest,
+	context: vscode.ChatContext,
+	stream: vscode.ChatResponseStream,
+	token: vscode.CancellationToken
+  ) => {
+  
+	try{
+		// send the request
+		const chatResponse = await ollama.chat({
+			model: 'qwen2.5-coder:1.5b',
+			messages: [{role: 'user', content: request.prompt}],
+			stream: true
+		});
+	  
+		// stream the response
+		for await (const fragment of chatResponse) {
+		  stream.markdown(fragment.message.content);
+		}
+
+	}catch(err){
+		const errorMessage = err instanceof Error ? err.message : String(err);
+
+		stream.markdown(errorMessage);
+		vscode.window.showErrorMessage(errorMessage); // Use error message instead of information
+	}
+  
+	return;
+  };
+  
+  // create participant
+  const coder = vscode.chat.createChatParticipant('codawy.codawy-coder', handler);
+  
+  // add icon to participant
+//   tutor.iconPath = vscode.Uri.joinPath(context.extensionUri, 'tutor.jpeg');
+  
+
+  // define a chat handler
+  const echoHandler: vscode.ChatRequestHandler = async (
+	request: vscode.ChatRequest,
+	context: vscode.ChatContext,
+	stream: vscode.ChatResponseStream,
+	token: vscode.CancellationToken
+  ) => {
+	stream.markdown(request.prompt);
+	
+	return;
+  };
+  
+  // create participant
+  const echoer = vscode.chat.createChatParticipant('codawy.codawy-echo', echoHandler);
+  
+  // add icon to participant
+//   tutor.iconPath = vscode.Uri.joinPath(context.extensionUri, 'tutor.jpeg');
+  
